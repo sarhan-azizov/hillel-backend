09f006aa863fc77c41a19a83a07dc92a4a6a7da0
saziz <S@rhan1988>
2021-01-25 17:14:53 +0200

    remove ability to create users by POST /users endpoint

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,41 +1,17 @@
-import { Controller, Body, Post, Get, UseGuards } from '@nestjs/common';
-import {
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-  ApiBody,
-  ApiBearerAuth,
-} from '@nestjs/swagger';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
 import { RolesGuard } from '../../shared/guards/authorization.guard';
 import { UserService } from './user.service';
 import { Roles } from '../../shared/decorators/roles.decorator';
 
-import {
-  CreateUserRequestDTO,
-  CreateUserResponseDTO,
-  GetUserResponseDTO,
-} from './dto';
+import { GetUserResponseDTO } from './dto';
 
 @ApiTags('Users')
 @UseGuards(RolesGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
-
-  @ApiOperation({ summary: 'Users' })
-  @ApiBody({ type: CreateUserRequestDTO })
-  @ApiResponse({
-    status: 201,
-    description: `Return created user`,
-    type: CreateUserResponseDTO,
-  })
-  @Post()
-  public async createUser(
-    @Body() createUserRequestDTO: CreateUserRequestDTO,
-  ): Promise<CreateUserResponseDTO> {
-    return await this.userService.createUser(createUserRequestDTO);
-  }
 
   @ApiBearerAuth()
   @Roles('admin')
